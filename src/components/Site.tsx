50be21eb51532b458faf7f135527f7b67d9ee2cd
NatKhomich <yalapulya94@mail.ru>
2023-04-11 13:00:50 +0600

    route. useParams

--- src/components/Site.tsx
+++ src/components/Site.tsx
@@ -5,6 +5,8 @@ import {PageOne} from './pages/PageOne';
 import {PageTwo} from './pages/PageTwo';
 import {PageThree} from './pages/PageThree';
 import {Error404} from './pages/Error404';
+import {Page} from './pages/Page';
+import {dataState} from '../data/DataState';
 
 
 export const Site = () => {
@@ -13,18 +15,25 @@ export const Site = () => {
             <div className={s.header}><h1>HEADER</h1></div>
             <div className={s.body}>
                 <div className={s.nav}>
-                    <div> <NavLink className={ ( {isActive} )=> isActive? s.active : s.navLink } to={'/page1'}> Первая </NavLink> </div>
+                    {/*<div> <NavLink className={ ( {isActive} )=> isActive? s.active : s.navLink } to={'/page1'}> Первая </NavLink> </div>
                     <div> <NavLink className={ ( {isActive} )=> isActive? s.active : s.navLink } to={'/page2'}> Вторая </NavLink> </div>
-                    <div> <NavLink className={ ( {isActive} )=> isActive? s.active : s.navLink } to={'/page3'}> Третья </NavLink> </div>
+                    <div> <NavLink className={ ( {isActive} )=> isActive? s.active : s.navLink } to={'/page3'}> Третья </NavLink> </div>*/}
+
+                    <div> <NavLink className={ ( {isActive} )=> isActive? s.active : s.navLink } to={'/page/0'}> Первая </NavLink> </div>
+                    <div> <NavLink className={ ( {isActive} )=> isActive? s.active : s.navLink } to={'/page/1'}> Вторая </NavLink> </div>
+                    <div> <NavLink className={ ( {isActive} )=> isActive? s.active : s.navLink } to={'/page/2'}> Третья </NavLink> </div>
+                    <div> <a href={'page/0'}> Нативка </a> </div>
                 </div>
                 <div className={s.content}>
                     <Routes>
 
-                        <Route path={'/'} element={<Navigate to={'/page1'}/>}/> //автоматическое вкл
+                        <Route path={'/'} element={<Navigate to={'/page0'}/>}/> //автоматическое вкл
 
-                        <Route path={'/page1'} element={<PageOne/>}/> //путь
+                        <Route path={'/page/:id'} element={<Page pages={dataState.pages}/>}/>
+
+                        {/*<Route path={'/page1'} element={<PageOne/>}/> //путь
                         <Route path={'/page2'} element={<PageTwo/>}/>
-                        <Route path={'/page3'} element={<PageThree/>}/>
+                        <Route path={'/page3'} element={<PageThree/>}/>*/}
 
                         <Route path={ '/*' } element={ <Error404/> } /> //ошибка
                     </Routes>
--- src/components/pages/Page.tsx
+++ src/components/pages/Page.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import {PagesType} from '../../data/DataState';
+import {useParams} from 'react-router-dom';
 
-export const Page = () => {
+type PagePropsType = {
+    pages: PagesType[]
+}
+
+export const Page = (props: PagePropsType) => {
+
+    const params = useParams()
+    console.log(Number(params.id) )
+
+
     return (
         <div>
-            Page
+            <div> {props.pages[Number(params.id)].heading} </div>
+            <div> {props.pages[Number(params.id)].about} </div>
         </div>
     );
 };
